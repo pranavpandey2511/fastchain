a090e7a072952c3926c9b4090b4cb9c81e93f1a1
Elvis Rari <elvorashs759@gmail.com>
2023-09-07 18:21:39 +0300

    added default batch size

--- fastchain/embeddings/base.py
+++ fastchain/embeddings/base.py
@@ -4,8 +4,7 @@ from typing import Callable, Optional, Tuple, Dict, List
 import numpy as np
 
 EMB_TYPE = np.ndarray
-
-
+DEFAULT_EMBED_BATCH_SIZE = 10
 class BaseEmbedding(ABC):
     def __init__(self, tokenizer: Optional[Callable] = None) -> None:
         self._total_tokens_used = 0
