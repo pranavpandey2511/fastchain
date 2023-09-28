0e3eb0fcfdbccb09e295c2bb8a8f97d1fa34b763
Pranav Pandey <pranavpandey2511@gmail.com>
2023-09-28 17:50:31 +0530

    Rebase from Aquib's changes

---
+++ fastchain/tests/test_chunker/test_code_chunker.py
@@ -0,0 +1,17 @@
+from fastchain.chunker.code_chunker import CodeChunker
+
+
+def test_code_chunker():
+    chunkern = CodeChunker(
+        directory_path="/Users/aquibkhan/Desktop/test_code_chunker",
+        max_chunk_size=500,
+        coalesce=50,
+    )
+    pages = chunkern.create_chunks()
+
+    for chunk in pages[0].chunks:
+        print("**************")
+        print(chunk)
+        print(chunk.content)
+        print(chunk.doc_id)
+        print("**************")
