d79152de7b4871de701684c5c663618e0badd9c0
Elvis Rari <elvorashs759@gmail.com>
2023-10-08 17:08:43 +0300

    fixed embeddings

--- fastchain/embedding/openai.py
+++ fastchain/embedding/openai.py
@@ -9,9 +9,13 @@ from tenacity import (
     stop_after_attempt,
     wait_random_exponential,
 )
-from fastchain.embeddings.base import DEFAULT_EMBED_BATCH_SIZE, BaseEmbedding
+from fastchain.embedding.base import DEFAULT_EMBED_BATCH_SIZE, BaseEmbedding
 from fastchain.llms.openai_utils import validate_openai_api_key
+from fastchain.document.base import Document
+import numpy as np
 
+EMB_TYPE = np.ndarray
+DEFAULT_EMBED_BATCH_SIZE = 10
 class OpenAIEmbeddingMode(str, Enum):
     """OpenAI embedding mode."""
     SIMILARITY_MODE = "similarity"
@@ -224,47 +228,51 @@ class OpenAIEmbedding(BaseEmbedding):
             If this value is not None, mode and model will be ignored.
             Only available for using AzureOpenAI.
     """
-
-    def __init__(
-        self,
-        mode: str = OpenAIEmbeddingMode.TEXT_SEARCH_MODE,
-        model: str = OpenAIEmbeddingModelType.TEXT_EMBED_ADA_002,
-        deployment_name: Optional[str] = None,
-        embed_batch_size: int = DEFAULT_EMBED_BATCH_SIZE,
-        tokenizer: Optional[Callable] = None,
-        **kwargs: Any,
-    ) -> None:
-        # Validate that either the openai.api_key property
-        # or OPENAI_API_KEY env variable are set to a valid key
-        # Raises ValueError if missing or doesn't match valid format
-        validate_openai_api_key(
-            kwargs.get("api_key", None), kwargs.get("api_type", None)
-        )
-
-        """Init params."""
-        super().__init__(embed_batch_size, tokenizer)
+    def __init__(self,
+                 mode: str = OpenAIEmbeddingMode.TEXT_SEARCH_MODE,
+                 model: str = OpenAIEmbeddingModelType.TEXT_EMBED_ADA_002,
+                 deployment_name: Optional[str] = None,
+                 embed_batch_size: int = DEFAULT_EMBED_BATCH_SIZE,
+                 tokenizer: Optional[Callable] = None,
+                 **kwargs: Any
+                 ) -> None:
+        super().__init__(tokenizer)
+        validate_openai_api_key(kwargs.get("api_key", None), kwargs.get("api_type", None))
+        
         self.deployment_name = deployment_name
         self.query_engine = get_engine(mode, model, _QUERY_MODE_MODEL_DICT)
         self.text_engine = get_engine(mode, model, _TEXT_MODE_MODEL_DICT)
         self.openai_kwargs = kwargs
 
-    def _get_query_embedding(self, query: str) -> List[float]:
-        """Get query embedding."""
-        return get_embedding(
-            query,
-            engine=self.query_engine,
-            deployment_id=self.deployment_name,
-            **self.openai_kwargs,
-        )
+    def _get_embedding(self, content: str) -> List[float]:
+        return self._get_text_embedding(content)
 
-    async def _aget_query_embedding(self, query: str) -> List[float]:
-        """The asynchronous version of _get_query_embedding."""
-        return await aget_embedding(
-            query,
-            engine=self.query_engine,
-            deployment_id=self.deployment_name,
-            **self.openai_kwargs,
-        )
+    async def _aget_embedding(self, content: str) -> List[float]:
+        return await self._aget_text_embedding(content)
+
+    def queue_document_for_embedding(self, document: Document) -> None:
+        self._document_queue.append(document)
+
+    def _process_documents(self, async_mode: bool = False) -> Dict[str, Dict[str, EMB_TYPE]]:
+        result = {}
+        for document in self._document_queue:
+            doc_embeddings = {}
+            for page in document.pages or []:
+                for chunk in page.chunks or []:
+                    emb_func = self.aget_chunk_embedding if async_mode else self.get_chunk_embedding
+                    doc_embeddings[chunk._id] = emb_func(chunk)
+            for chunk in document.chunks or []:
+                emb_func = self.aget_chunk_embedding if async_mode else self.get_chunk_embedding
+                doc_embeddings[chunk._id] = emb_func(chunk)
+            result[document._id] = doc_embeddings
+        self._document_queue.clear()
+        return result
+
+    def get_queued_document_embeddings(self) -> Dict[str, Dict[str, EMB_TYPE]]:
+        return self._process_documents()
+
+    async def aget_queued_document_embeddings(self) -> Dict[str, Dict[str, EMB_TYPE]]:
+        return await self._process_documents(async_mode=True)
 
     def _get_text_embedding(self, text: str) -> List[float]:
         """Get text embedding."""
@@ -272,7 +280,7 @@ class OpenAIEmbedding(BaseEmbedding):
             text,
             engine=self.text_engine,
             deployment_id=self.deployment_name,
-            **self.openai_kwargs,
+            **self.openai_kwargs
         )
 
     async def _aget_text_embedding(self, text: str) -> List[float]:
@@ -281,21 +289,16 @@ class OpenAIEmbedding(BaseEmbedding):
             text,
             engine=self.text_engine,
             deployment_id=self.deployment_name,
-            **self.openai_kwargs,
+            **self.openai_kwargs
         )
 
     def _get_text_embeddings(self, texts: List[str]) -> List[List[float]]:
-        """Get text embeddings.
-
-        By default, this is a wrapper around _get_text_embedding.
-        Can be overriden for batch queries.
-
-        """
+        """Get text embeddings."""
         return get_embeddings(
             texts,
             engine=self.text_engine,
             deployment_id=self.deployment_name,
-            **self.openai_kwargs,
+            **self.openai_kwargs
         )
 
     async def _aget_text_embeddings(self, texts: List[str]) -> List[List[float]]:
@@ -304,5 +307,5 @@ class OpenAIEmbedding(BaseEmbedding):
             texts,
             engine=self.text_engine,
             deployment_id=self.deployment_name,
-            **self.openai_kwargs,
+            **self.openai_kwargs
         )
